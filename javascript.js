373b08f4591e2a6fece9232d339212911fb1a83c
julian <julian_stanger@web.de>
2022-12-25 16:05:33 +0100

    Add javascript to fill canvas with squares

--- javascript.js
+++ javascript.js
@@ -1,11 +1,30 @@
 function fillCanvas() {
+  squares = prompt("How many squares do you want?");
+  root = Math.sqrt(squares);
+
+  // check if is a square number
+  if (Number.isInteger(root) == false) {
+    alert("Not a square number!");
+    return
+  }
+  
+  // remove all existing squares
   const canvas = document.querySelector('#canvas');
+  canvas.innerHTML = '';
 
-  const column = document.createElement('div');
-  column.classList.add('column');
+  for (let i = 0; i < root; i++) {
+    const column = document.createElement('div');
+    column.classList.add('col');
 
-  canvas.appendChild(column);
-  alert("Yes");
+    // create (square amount) of rows for each column
+    for (let j = 0; j < root; j++) {
+      const row = document.createElement('row');
+      row.classList.add('row');
+      column.appendChild(row);
+    }
+
+    canvas.appendChild(column);
+  }
 }
 
 const btn = document.querySelector('#init');
